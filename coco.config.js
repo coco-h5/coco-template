09797f5db98b97e9c9dfd2ba95912b74963ed1c7
muwoo <2424880409@qq.com>
2021-03-02 10:31:59 +0800

    ref: 支持远程组件

---
+++ coco.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'coco-template',
+  templateName: '营销活动',
+  author: 'muwoo',
+  snapshot: 'https://cdn.img.snapshot.png',
+  gitUrl: 'git@gitlab.yourgitlab.com:coco/coco-template.git',
+  projectType: '0'
+};
