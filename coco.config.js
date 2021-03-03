5d355f98ceff1c4d0d66457570f24b8f5c9dda96
muwoo <2424880409@qq.com>
2021-03-03 10:11:25 +0800

    release project

--- coco.config.js
+++ coco.config.js
@@ -3,6 +3,6 @@ module.exports = {
   templateName: '营销活动',
   author: 'muwoo',
   snapshot: 'https://cdn.img.snapshot.png',
-  gitUrl: 'git@gitlab.yourgitlab.com:coco/coco-template.git',
+  gitUrl: 'git@github.com:coco-h5/coco-template.git',
   projectType: '0'
 };
