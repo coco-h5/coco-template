4cd2f2d250d46462d56db93293db5d894508dd3c
muwoo <2424880409@qq.com>
2021-03-03 18:35:29 +0800

    release project

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <CocoComponent>
       <coco-banner :obj="{
-        src: require('./assets/banner.jpg'),
+        src: 'https://p3-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/a2e81c9d78a24c5fb0cb33bfcc366dce~tplv-k3u1fbpfcp-watermark.image',
         link: 'https://coco.com',
       }" />
       <coco-form />
