117a54ef104fac78f8b3fd331ef127e3867a5a40
muwoo <2424880409@qq.com>
2021-09-14 11:48:32 +0800

    demo

--- coco.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: 'coco-template',
-  templateName: '营销活动',
-  author: 'muwoo',
-  snapshot: 'https://cdn.img.snapshot.png',
-  gitUrl: 'coco-h5/coco-template',
-  projectType: '0'
-};
